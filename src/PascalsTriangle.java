72b4945bd1f979bb4d1e361305d70136bedef493
Cornel <140949562+Cornel0210@users.noreply.github.com>
2023-08-18 17:11:38 +0300

    update project

--- src/PascalsTriangle.java
+++ src/PascalsTriangle.java
@@ -2,9 +2,22 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Given an integer numRows, return the first numRows of Pascal's triangle.
+ * In Pascal's triangle, each number is the sum of the two numbers directly above.
+ *
+ * Example 1:
+ * Input: numRows = 5
+ * Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+ *
+ * Example 2:
+ * Input: numRows = 1
+ * Output: [[1]]
+ */
 public class PascalsTriangle {
     public static void main(String[] args) {
         System.out.println(generate(5));
+        System.out.println(generate(10));
     }
     public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> pascalsTriangle = new LinkedList<>();
